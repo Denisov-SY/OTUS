f7361b287468982417948b93f2d899907c795b1b
Denisov-SY <densq@mail.ru>
2024-11-07 22:17:25 +0300

    home work 01

---
+++ lesson_01/src/components/Comp1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="header">Component 1</h2>
+    <p>{{ msg }}</p>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+.header {
+  color: blue;
+}
+</style>
---
+++ lesson_01/src/components/Comp2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="header">Component 2</h2>
+    <p>{{ msg }}</p>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+.header {
+  color: red;
+}
+</style>
